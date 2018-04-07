28c72410bc5e5878e0c146f42d69203cc552e3be
Chris van Zomeren <vanzomerenc@gmail.com>
2018-04-07 10:50:19 -0400

    Separate signal analysis from simulation

--- top_block.py
+++ top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Sat Apr  7 09:17:12 2018
+# Generated: Sat Apr  7 10:43:46 2018
 ##################################################
 
 if __name__ == '__main__':
@@ -21,16 +21,14 @@ from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import channels
 from gnuradio import eng_notation
-from gnuradio import fft
+from gnuradio import filter
 from gnuradio import gr
 from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
-from gnuradio.fft import window
 from gnuradio.filter import firdes
 from gnuradio.qtgui import Range, RangeWidget
 from optparse import OptionParser
 import DirectionalAntenna
-import collar_detect
 import math
 import scipy
 import sip
@@ -75,8 +73,8 @@ class top_block(gr.top_block, Qt.QWidget):
         self.tuner_freq = tuner_freq = 150000000
         self.receiver_samp_rate = receiver_samp_rate = 6000000
         self.pulse_duty_ticks = pulse_duty_ticks = int(math.floor(pulse_duty * pulse_period_ticks))
+        self.network_decim = network_decim = 16
         self.freq = freq = 150000000
-        self.display_scale = display_scale = 0
         self.direction = direction = 0
         self.SNR = SNR = 50
 
@@ -89,51 +87,31 @@ class top_block(gr.top_block, Qt.QWidget):
         self._freq_range = Range(148000000, 152000000, 100000, 150000000, 200)
         self._freq_win = RangeWidget(self._freq_range, self.set_freq, 'Freq', "counter_slider", float)
         self.top_layout.addWidget(self._freq_win)
-        self._display_scale_range = Range(0, 100, 1, 0, 200)
-        self._display_scale_win = RangeWidget(self._display_scale_range, self.set_display_scale, "display_scale", "dial", float)
-        self.top_layout.addWidget(self._display_scale_win)
         self._direction_range = Range(0, 360, 1, 0, 1)
         self._direction_win = RangeWidget(self._direction_range, self.set_direction, 'Direction', "counter_slider", float)
         self.top_layout.addWidget(self._direction_win)
         self._SNR_range = Range(0, 100, 1, 50, 200)
         self._SNR_win = RangeWidget(self._SNR_range, self.set_SNR, "SNR", "counter_slider", float)
         self.top_layout.addWidget(self._SNR_win)
-        self.qtgui_vector_sink_f_0 = qtgui.vector_sink_f(
-            512,
-            0,
-            1.0,
-            "x-Axis",
-            "y-Axis",
-            "",
-            1 # Number of inputs
+        self.qtgui_sink_x_1 = qtgui.sink_c(
+        	1024, #fftsize
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	tuner_freq, #fc
+        	receiver_samp_rate / network_decim, #bw
+        	"", #name
+        	True, #plotfreq
+        	True, #plotwaterfall
+        	True, #plottime
+        	True, #plotconst
         )
-        self.qtgui_vector_sink_f_0.set_update_time(0.10)
-        self.qtgui_vector_sink_f_0.set_y_axis(0, 10**(display_scale/10))
-        self.qtgui_vector_sink_f_0.enable_autoscale(False)
-        self.qtgui_vector_sink_f_0.enable_grid(False)
-        self.qtgui_vector_sink_f_0.set_x_axis_units("")
-        self.qtgui_vector_sink_f_0.set_y_axis_units("")
-        self.qtgui_vector_sink_f_0.set_ref_level(0)
+        self.qtgui_sink_x_1.set_update_time(1.0/10)
+        self._qtgui_sink_x_1_win = sip.wrapinstance(self.qtgui_sink_x_1.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_sink_x_1_win)
 
-        labels = ['', '', '', '', '',
-                  '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-                  1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-                  "magenta", "yellow", "dark red", "dark green", "dark blue"]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
-        for i in xrange(1):
-            if len(labels[i]) == 0:
-                self.qtgui_vector_sink_f_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_vector_sink_f_0.set_line_label(i, labels[i])
-            self.qtgui_vector_sink_f_0.set_line_width(i, widths[i])
-            self.qtgui_vector_sink_f_0.set_line_color(i, colors[i])
-            self.qtgui_vector_sink_f_0.set_line_alpha(i, alphas[i])
+        self.qtgui_sink_x_1.enable_rf_freq(False)
 
-        self._qtgui_vector_sink_f_0_win = sip.wrapinstance(self.qtgui_vector_sink_f_0.pyqwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_vector_sink_f_0_win)
+
+
         self.qtgui_sink_x_0 = qtgui.sink_c(
         	1024, #fftsize
         	firdes.WIN_BLACKMAN_hARRIS, #wintype
@@ -163,20 +141,18 @@ class top_block(gr.top_block, Qt.QWidget):
         self._pulse_duty_range = Range(0, 1, 0.001, 0.2, 200)
         self._pulse_duty_win = RangeWidget(self._pulse_duty_range, self.set_pulse_duty, "pulse_duty", "counter_slider", float)
         self.top_layout.addWidget(self._pulse_duty_win)
-        self.fft_vxx_0 = fft.fft_vfc(512, True, (window.rectangular(512)), 1)
-        self.collar_detect = collar_detect.collar_detect()
+        self.low_pass_filter_0 = filter.fir_filter_ccf(network_decim, firdes.low_pass(
+        	1, receiver_samp_rate, receiver_samp_rate / network_decim / 2, 1000, firdes.WIN_RECTANGULAR, 6.76))
         self.channels_quantizer_0_0 = channels.quantizer(10)
         self.channels_quantizer_0 = channels.quantizer(10)
         self.blocks_vector_source_x_0 = blocks.vector_source_c([1]*pulse_duty_ticks + [0]*(pulse_period_ticks-pulse_duty_ticks), True, 1, [])
+        self.blocks_udp_sink_0 = blocks.udp_sink(gr.sizeof_gr_complex*1024, 'localhost', 39253, 1472, True)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, receiver_samp_rate,True)
         self.blocks_sub_xx_0 = blocks.sub_cc(1)
-        self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_float*1, 512)
+        self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, 1024)
         self.blocks_repeat_0 = blocks.repeat(gr.sizeof_gr_complex*1, receiver_samp_rate / pulse_samp_rate)
-        self.blocks_multiply_xx_1 = blocks.multiply_vcc(512)
         self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
         self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
-        self.blocks_complex_to_real_0 = blocks.complex_to_real(1)
-        self.blocks_complex_to_mag_0 = blocks.complex_to_mag(512)
         self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
         self.blocks_add_xx_1 = blocks.add_vcc(1)
         self.blocks_add_xx_0 = blocks.add_vcc(1)
@@ -199,22 +175,18 @@ class top_block(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_add_xx_1, 0), (self.blocks_complex_to_float_0, 0))
         self.connect((self.blocks_complex_to_float_0, 0), (self.channels_quantizer_0, 0))
         self.connect((self.blocks_complex_to_float_0, 1), (self.channels_quantizer_0_0, 0))
-        self.connect((self.blocks_complex_to_mag_0, 0), (self.collar_detect, 0))
-        self.connect((self.blocks_complex_to_mag_0, 0), (self.qtgui_vector_sink_f_0, 0))
-        self.connect((self.blocks_complex_to_real_0, 0), (self.blocks_stream_to_vector_0, 0))
         self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_sub_xx_0, 0))
         self.connect((self.blocks_multiply_xx_0, 0), (self.DirectionalAntenna, 1))
-        self.connect((self.blocks_multiply_xx_1, 0), (self.blocks_complex_to_mag_0, 0))
         self.connect((self.blocks_repeat_0, 0), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.blocks_stream_to_vector_0, 0), (self.fft_vxx_0, 0))
-        self.connect((self.blocks_sub_xx_0, 0), (self.blocks_complex_to_real_0, 0))
+        self.connect((self.blocks_stream_to_vector_0, 0), (self.blocks_udp_sink_0, 0))
+        self.connect((self.blocks_sub_xx_0, 0), (self.low_pass_filter_0, 0))
         self.connect((self.blocks_sub_xx_0, 0), (self.qtgui_sink_x_0, 0))
         self.connect((self.blocks_throttle_0, 0), (self.blocks_multiply_xx_0, 0))
         self.connect((self.blocks_vector_source_x_0, 0), (self.blocks_repeat_0, 0))
         self.connect((self.channels_quantizer_0, 0), (self.blocks_float_to_complex_0, 0))
         self.connect((self.channels_quantizer_0_0, 0), (self.blocks_float_to_complex_0, 1))
-        self.connect((self.fft_vxx_0, 0), (self.blocks_multiply_xx_1, 0))
-        self.connect((self.fft_vxx_0, 0), (self.blocks_multiply_xx_1, 1))
+        self.connect((self.low_pass_filter_0, 0), (self.blocks_stream_to_vector_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.qtgui_sink_x_1, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "top_block")
@@ -263,6 +235,7 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_tuner_freq(self, tuner_freq):
         self.tuner_freq = tuner_freq
+        self.qtgui_sink_x_1.set_frequency_range(self.tuner_freq, self.receiver_samp_rate / self.network_decim)
         self.qtgui_sink_x_0.set_frequency_range(self.tuner_freq, self.receiver_samp_rate)
         self.analog_sig_source_x_0.set_frequency(self.freq - self.tuner_freq)
 
@@ -271,7 +244,9 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_receiver_samp_rate(self, receiver_samp_rate):
         self.receiver_samp_rate = receiver_samp_rate
+        self.qtgui_sink_x_1.set_frequency_range(self.tuner_freq, self.receiver_samp_rate / self.network_decim)
         self.qtgui_sink_x_0.set_frequency_range(self.tuner_freq, self.receiver_samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.receiver_samp_rate, self.receiver_samp_rate / self.network_decim / 2, 1000, firdes.WIN_RECTANGULAR, 6.76))
         self.blocks_throttle_0.set_sample_rate(self.receiver_samp_rate)
         self.blocks_repeat_0.set_interpolation(self.receiver_samp_rate / self.pulse_samp_rate)
         self.analog_sig_source_x_0.set_sampling_freq(self.receiver_samp_rate)
@@ -283,19 +258,20 @@ class top_block(gr.top_block, Qt.QWidget):
         self.pulse_duty_ticks = pulse_duty_ticks
         self.blocks_vector_source_x_0.set_data([1]*self.pulse_duty_ticks + [0]*(self.pulse_period_ticks-self.pulse_duty_ticks), [])
 
+    def get_network_decim(self):
+        return self.network_decim
+
+    def set_network_decim(self, network_decim):
+        self.network_decim = network_decim
+        self.qtgui_sink_x_1.set_frequency_range(self.tuner_freq, self.receiver_samp_rate / self.network_decim)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.receiver_samp_rate, self.receiver_samp_rate / self.network_decim / 2, 1000, firdes.WIN_RECTANGULAR, 6.76))
+
     def get_freq(self):
         return self.freq
 
     def set_freq(self, freq):
         self.freq = freq
         self.analog_sig_source_x_0.set_frequency(self.freq - self.tuner_freq)
-
-    def get_display_scale(self):
-        return self.display_scale
-
-    def set_display_scale(self, display_scale):
-        self.display_scale = display_scale
-        self.qtgui_vector_sink_f_0.set_y_axis(0, 10**(self.display_scale/10))
 
     def get_direction(self):
         return self.direction
