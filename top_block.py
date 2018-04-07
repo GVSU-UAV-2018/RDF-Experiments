051b7a350f8a795dd23c8692b5392ab3441a5a49
Chris van Zomeren <vanzomerenc@gmail.com>
2018-04-07 17:31:06 -0400

    Add audio output for simulation, and adjust noise multiplier

--- top_block.py
+++ top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Sat Apr  7 15:26:43 2018
+# Generated: Sat Apr  7 17:25:19 2018
 ##################################################
 
 if __name__ == '__main__':
@@ -18,6 +18,7 @@ if __name__ == '__main__':
 
 from PyQt4 import Qt
 from gnuradio import analog
+from gnuradio import audio
 from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import filter
@@ -74,11 +75,11 @@ class top_block(gr.top_block, Qt.QWidget):
         self.pulse_duty_1 = pulse_duty_1 = 0.2
         self.pulse_duty_0 = pulse_duty_0 = 0.2
         self.tuner_freq = tuner_freq = 150000000
-        self.receiver_samp_rate = receiver_samp_rate = 6000000
+        self.receiver_samp_rate = receiver_samp_rate = 3000000
         self.pulse_samp_rate_1 = pulse_samp_rate_1 = 1000
         self.pulse_duty_ticks_1 = pulse_duty_ticks_1 = int(math.floor(pulse_duty_1 * pulse_period_ticks_1))
         self.pulse_duty_ticks_0 = pulse_duty_ticks_0 = int(math.floor(pulse_duty_0 * pulse_period_ticks_0))
-        self.noise_correction_factor = noise_correction_factor = 30
+        self.noise_correction_factor = noise_correction_factor = 100
         self.network_decim = network_decim = 16
         self.n_adc_bits = n_adc_bits = 10.4
         self.freq_1 = freq_1 = 150000000
@@ -90,7 +91,7 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self._tuner_freq_range = Range(148000000, 152000000, 1, 150000000, 200)
+        self._tuner_freq_range = Range(148000000, 152000000, 10000, 150000000, 200)
         self._tuner_freq_win = RangeWidget(self._tuner_freq_range, self.set_tuner_freq, "tuner_freq", "counter_slider", float)
         self.top_layout.addWidget(self._tuner_freq_win)
         self._freq_1_range = Range(148000000, 152000000, 100000, 150000000, 200)
@@ -128,25 +129,6 @@ class top_block(gr.top_block, Qt.QWidget):
 
 
 
-        self.qtgui_sink_x_0 = qtgui.sink_c(
-        	1024, #fftsize
-        	firdes.WIN_BLACKMAN_hARRIS, #wintype
-        	tuner_freq, #fc
-        	receiver_samp_rate, #bw
-        	"", #name
-        	True, #plotfreq
-        	True, #plotwaterfall
-        	True, #plottime
-        	True, #plotconst
-        )
-        self.qtgui_sink_x_0.set_update_time(1.0/10)
-        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_sink_x_0_win)
-
-        self.qtgui_sink_x_0.enable_rf_freq(True)
-
-
-
         self._pulse_period_1_tool_bar = Qt.QToolBar(self)
         self._pulse_period_1_tool_bar.addWidget(Qt.QLabel('Signal 1 Period (s)'+": "))
         self._pulse_period_1_line_edit = Qt.QLineEdit(str(self.pulse_period_1))
@@ -169,6 +151,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self.top_layout.addWidget(self._pulse_duty_0_win)
         self.low_pass_filter_0 = filter.fir_filter_ccf(network_decim, firdes.low_pass(
         	1, receiver_samp_rate, receiver_samp_rate / network_decim / 2, 1000, firdes.WIN_RECTANGULAR, 6.76))
+        self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_ccc(2, (20, ), 24000, receiver_samp_rate)
         self.blocks_vector_source_x_0_1 = blocks.vector_source_c([1]*pulse_duty_ticks_1 + [0]*(pulse_period_ticks_1-pulse_duty_ticks_1), True, 1, [])
         self.blocks_vector_source_x_0 = blocks.vector_source_c([1]*pulse_duty_ticks_0 + [0]*(pulse_period_ticks_0-pulse_duty_ticks_0), True, 1, [])
         self.blocks_udp_sink_0 = blocks.udp_sink(gr.sizeof_gr_complex*1024, 'localhost', 39253, 1472, True)
@@ -179,9 +162,12 @@ class top_block(gr.top_block, Qt.QWidget):
         self.blocks_repeat_0 = blocks.repeat(gr.sizeof_gr_complex*1, receiver_samp_rate / pulse_samp_rate)
         self.blocks_multiply_xx_0_1 = blocks.multiply_vcc(1)
         self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vcc((0.002, ))
+        self.blocks_complex_to_real_0 = blocks.complex_to_real(1)
         self.blocks_add_xx_0 = blocks.add_vcc(1)
         self.blocks_add_const_vxx_1 = blocks.add_const_vff((direction_1, ))
         self.blocks_add_const_vxx_0 = blocks.add_const_vff((direction_0, ))
+        self.audio_sink_0 = audio.sink(48000, '', True)
         self.analog_sig_source_x_0_1 = analog.sig_source_c(receiver_samp_rate, analog.GR_COS_WAVE, freq_1 - tuner_freq, 1, 0)
         self.analog_sig_source_x_0 = analog.sig_source_c(receiver_samp_rate, analog.GR_COS_WAVE, freq_0 - tuner_freq, 1, 0)
         self.analog_noise_source_x_1 = analog.noise_source_c(analog.GR_UNIFORM, noise_correction_factor/2**n_adc_bits, 0)
@@ -201,7 +187,8 @@ class top_block(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_add_const_vxx_0, 0), (self.DirectionalAntenna, 0))
         self.connect((self.blocks_add_const_vxx_1, 0), (self.DirectionalAntenna_1, 0))
         self.connect((self.blocks_add_xx_0, 0), (self.low_pass_filter_0, 0))
-        self.connect((self.blocks_add_xx_0, 0), (self.qtgui_sink_x_0, 0))
+        self.connect((self.blocks_complex_to_real_0, 0), (self.audio_sink_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_complex_to_real_0, 0))
         self.connect((self.blocks_multiply_xx_0, 0), (self.DirectionalAntenna, 1))
         self.connect((self.blocks_multiply_xx_0_1, 0), (self.DirectionalAntenna_1, 1))
         self.connect((self.blocks_repeat_0, 0), (self.blocks_multiply_xx_0, 1))
@@ -211,7 +198,9 @@ class top_block(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_throttle_0_1, 0), (self.blocks_multiply_xx_0_1, 0))
         self.connect((self.blocks_vector_source_x_0, 0), (self.blocks_repeat_0, 0))
         self.connect((self.blocks_vector_source_x_0_1, 0), (self.blocks_repeat_0_1, 0))
+        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.blocks_multiply_const_vxx_0, 0))
         self.connect((self.low_pass_filter_0, 0), (self.blocks_stream_to_vector_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.freq_xlating_fir_filter_xxx_0, 0))
         self.connect((self.low_pass_filter_0, 0), (self.qtgui_sink_x_1, 0))
         self.connect((self.udp_repeat_source, 0), (self.blocks_add_const_vxx_0, 0))
         self.connect((self.udp_repeat_source, 0), (self.blocks_add_const_vxx_1, 0))
@@ -283,7 +272,6 @@ class top_block(gr.top_block, Qt.QWidget):
     def set_tuner_freq(self, tuner_freq):
         self.tuner_freq = tuner_freq
         self.qtgui_sink_x_1.set_frequency_range(self.tuner_freq, self.receiver_samp_rate / self.network_decim)
-        self.qtgui_sink_x_0.set_frequency_range(self.tuner_freq, self.receiver_samp_rate)
         self.analog_sig_source_x_0_1.set_frequency(self.freq_1 - self.tuner_freq)
         self.analog_sig_source_x_0.set_frequency(self.freq_0 - self.tuner_freq)
 
@@ -293,7 +281,6 @@ class top_block(gr.top_block, Qt.QWidget):
     def set_receiver_samp_rate(self, receiver_samp_rate):
         self.receiver_samp_rate = receiver_samp_rate
         self.qtgui_sink_x_1.set_frequency_range(self.tuner_freq, self.receiver_samp_rate / self.network_decim)
-        self.qtgui_sink_x_0.set_frequency_range(self.tuner_freq, self.receiver_samp_rate)
         self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.receiver_samp_rate, self.receiver_samp_rate / self.network_decim / 2, 1000, firdes.WIN_RECTANGULAR, 6.76))
         self.blocks_throttle_0_1.set_sample_rate(self.receiver_samp_rate)
         self.blocks_throttle_0.set_sample_rate(self.receiver_samp_rate)
